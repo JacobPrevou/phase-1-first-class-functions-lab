cd07fb97616734ba16ac0bdff199d07f40aad472
Jake <prevou15@gmail.com>
2021-09-19 05:04:31 -0500

    commit tree cecacd8b4015e27b42421374edaf761f26616d94

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+  
+
+const returnFirstTwoDrivers = function(drivers) {
+    return ['Sally', 'Bob'];
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return ['Freddy', 'Claudia'];
+}
+
+const selectingDrivers =  [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function() {
+    return function fareQuintupler(fare) {
+        return (fare * 5)
+    };
+};
+
+const fareDoubler = function(fare) {
+    return (fare * 2);
+}
+
+const fareTripler = function(fare) {
+    return (fare * 3);
+}
+
+const selectDifferentDrivers = function() {
+    return returnFirstTwoDrivers(drivers);
+}
+
+// describe('selectDifferentDrivers(arrayOfDrivers, function)', function () {
+//     it('returns the first two drivers when passed returnFirstTwoDrivers() as the second argument', function () {
+//       expect(selectDifferentDrivers(drivers, returnFirstTwoDrivers)).to.eql(['Sally', 'Bob']);
+//     });
+
+//     it('returns the last two drivers when passed returnLastTwoDrivers() as the second argument', function () {
+//       expect(selectDifferentDrivers(drivers, returnLastTwoDrivers)).to.eql(['Freddy', 'Claudia']);
+//     });
+
+//slice
